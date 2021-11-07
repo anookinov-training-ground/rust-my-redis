3adc9562799640eeb66505497cc7c6ec283c6663
Alexander Nookinov <anookinov@gmail.com>
2021-11-07 20:49:36 +0700

    move echo client code to echo-client.rs and add echo server code into echo-server-copy.rs

--- src/bin/echo-server-copy.rs
+++ src/bin/echo-server-copy.rs
@@ -1,32 +1,20 @@
-use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
-use tokio::net::TcpStream;
+use tokio::io;
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let socket = TcpStream::connect("127.0.0.1:6142").await?;
-    let (mut rd, mut wr) = io::split(socket);
-
-    // Write data in the background
-    tokio::spawn(async move {
-        wr.write_all(b"hello\r\n").await?;
-        wr.write_all(b"world\r\n").await?;
-
-        // Sometimes, the rust type inferencer needs
-        // a little help
-        Ok::<_, io::Error>(())
-    });
-
-    let mut buf = vec![0; 128];
+    let listener = TcpListener::bind("127.0.0.1:6142").await?;
 
     loop {
-        let n = rd.read(&mut buf).await?;
+        let (mut socket, _) = listener.accept().await?;
 
-        if n == 0 {
-            break;
-        }
+        tokio::spawn(async move {
+            // Copy data here
+            let (mut rd, mut wr) = socket.split();
 
-        println!("GOT {:?}", &buf[..n]);
+            if io::copy(&mut rd, &mut wr).await.is_err() {
+                eprintln!("failed to copy");
+            }
+        });
     }
-
-    Ok(())
 }
